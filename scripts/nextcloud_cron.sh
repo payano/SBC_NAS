a850dc8d8bb3085ef4594ef80f89669983bcc38d
Johan Svensson <payano@users.noreply.github.com>
2021-04-28 21:11:25 +0200

    Update nextcloud_cron.sh

--- scripts/nextcloud_cron.sh
+++ scripts/nextcloud_cron.sh
@@ -2,7 +2,7 @@
 
 DOCKER_ID=$(docker ps | awk '{print $1" "$2}' | grep nextcloud | awk '{print $1}')
 
-echo "starting..." >> /tmp/out.txt
-echo $DOCKER_ID >> /tmp/out.txt
+echo "starting..." > /tmp/out.txt
+echo $DOCKER_ID > /tmp/out.txt
 RUNNED=$(/usr/bin/docker exec -u 33 $DOCKER_ID ./occ files:scan --all)
-echo $RUNNED >> /tmp/out.txt
+echo $RUNNED > /tmp/out.txt
