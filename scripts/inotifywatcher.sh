3cbc86b98e531f0fa01395789295d52e6350726c
Johan <johan@exclude.se>
2021-05-02 17:37:54 +0000

    change order of chmod and chown

--- scripts/inotifywatcher.sh
+++ scripts/inotifywatcher.sh
@@ -7,19 +7,19 @@ WATCH_DIR="${MOUNTPOINT}/files"
 if [ ! -d "$WATCH_DIR" ]
 then
         mkdir -p $WATCH_DIR
-	chown www-data:www-data $WATCH_DIR
 	chmod 0775 $WATCH_DIR
+	chown www-data:www-data $WATCH_DIR
 fi
 
 inotifywait -q -r -m -e create $WATCH_DIR |
 while read -r dir event file; do
 	if [ -f "${dir}${file}" ]
 	then
-		chown www-data:www-data ${dir}${file}
 		chmod 0664 ${dir}${file}
+		chown www-data:www-data ${dir}${file}
 	elif [ -d "${dir}${file}" ]
 	then
-		chown www-data:www-data ${dir}${file}
 		chmod 0775 ${dir}${file}
+		chown www-data:www-data ${dir}${file}
 	fi
 done
