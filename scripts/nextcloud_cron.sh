cf9f42967a7712a324ac650307d997eea52048e8
Johan Svensson <johan@exclude.se>
2021-11-06 22:32:41 +0100

    Nextcloud: Add cron.php to be runned

--- scripts/nextcloud_cron.sh
+++ scripts/nextcloud_cron.sh
@@ -5,4 +5,15 @@ DOCKER_ID=$(docker ps | awk '{print $1" "$2}' | grep nextcloud | awk '{print $1}
 echo "starting..." > /tmp/out.txt
 echo $DOCKER_ID > /tmp/out.txt
 RUNNED=$(/usr/bin/docker exec -u 33 $DOCKER_ID ./occ files:scan --all)
-echo $RUNNED > /tmp/out.txt
+if [ $? -ne 0 ]
+then
+	echo "couldn't execute the docker scan files"
+fi
+
+RUNNED=$(/usr/bin/docker exec -u 33 $DOCKER_ID php ./cron.php)
+if [ $? -ne 0 ]
+then
+	echo "couldn't execute the docker cron files"
+fi
+
+
